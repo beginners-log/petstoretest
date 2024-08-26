10f424a2b58cab88db8f77cae3344643a88a1dbc
beginners-log <oxbaxmann@gmail.com>
2024-08-26 12:28:01 +0300

    Add mochawesome-reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress'
 import fs from 'fs-extra'
 import path from 'path'
+import pluginMocha from 'cypress-mochawesome-reporter/plugin.js'
 
 const getConfigurationByFile = (file) => {
   const pathToConfigFile = path.resolve('config', `${file}.json`)
@@ -9,8 +10,14 @@ const getConfigurationByFile = (file) => {
 }
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: `reports_${new Date().toLocaleString().replace(',', '')}`
+  },
   e2e: {
     setupNodeEvents(on, config) {
+      pluginMocha(on)
       const file = config.env.configFile || 'dev'
 
       return getConfigurationByFile(file)
